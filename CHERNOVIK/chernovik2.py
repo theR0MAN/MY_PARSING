e3a75d347bbf46d15adc736634f6d3f874ffd423
theR0MAN <roman.miliuta@gmail.com>
2022-09-18 14:49:12 +0300

    Tik 001 в черновик2 добавим функцию plus

--- CHERNOVIK/chernovik2.py
+++ CHERNOVIK/chernovik2.py
@@ -1,6 +1,12 @@
 
 a=['2','2','4','5','6']
 
+
+def plus (a,b):
+    return a+b
+
+
+
 b=a.copy()
 c=list(0 for i in range(10))
 k=0
@@ -10,5 +16,6 @@ for i in a:
 
 print(b)
 print(c)
+print( plus(10,10))
 
 
