ba559fe28571d06b1fb66160e876975d555914ac
PythonLearningJun <miliuta.roman@yandex.by>
2022-10-01 22:19:24 +0300

    мультипоточный джсон перепарсер

--- CHERNOVIK/perepars_to_json_fonda.py
+++ CHERNOVIK/perepars_to_json_fonda.py
@@ -5,54 +5,46 @@ import time
 import os
 import datetime
 
+
 def end_func(response):
-    print("end_func:",response)
+    print("end_func:", response)
 
 
 getpath = '/media/roman/J/OLDHIST/FONDA'
+path2 = '/media/roman/J/jsOLDHIST/FONDA'
 
-start_year = 2020
-start_month = 4
-start_day = 2
+start_year = 2019
+start_month = 1
+start_day = 1
 start_hour = 10
 
-stop_year = 2020
-stop_month = 4
-stop_day = 2
-stop_hour = 10
+stop_year = 2021
+stop_month = 1
+stop_day = 22
+stop_hour = 17
 
 content = getdata(getpath, start_year, start_month, start_day, start_hour, stop_year, stop_month, stop_day, stop_hour)
 print(content)
 
 
 def perepars(i):
-
-    bigdict={}
-    biglist=[]
-    timekey=''
-
+    filename = i
+    bigdict = {}
+    biglist = []
+    timekey = ''
     with open(i, mode='r', encoding='utf-8') as r:
         zl = r.readlines()
-
-
     for i in zl:  # пробегаеся по списку строк
         a = i.split()  # дробим каждую строку в элементы списка
-        # print(a)
         if len(a) > 1:
-
-            if(a[0]=='*'):
-                # print(a)
+            if (a[0] == '*'):
                 if timekey > '':
-                    # bigdict.update(timekey=biglist)
-                    bigdict[timekey]=biglist
-                    # print(biglist)
-                timekey=a[1]
+                    bigdict[timekey] = biglist
+                timekey = a[1]
                 biglist = []
             if len(a) > 2:
-
                 asks = []
                 bids = []
-
                 ind = 11
                 for i in range(int(a[10])):
                     asks.append((float(a[ind]), float(a[ind + 1])))
@@ -60,20 +52,37 @@ def perepars(i):
                 for i in range(int(a[ind])):
                     bids.append((float(a[ind + 1]), float(a[ind + 2])))
                     ind += 2
-
                 if len(asks) > 0 and len(bids) > 0:
                     ask = asks[0][0]
                     bid = bids[0][0]
-                    dat = dict(i=a[1], p=a[0], l=float(a[4]), a=float(ask), b=float(bid), vl=float(a[5]), bvl=float(a[6]),
+                    dat = dict(i=a[1], p=a[0], l=float(a[4]), a=float(ask), b=float(bid), vl=float(a[5]),
+                               bvl=float(a[6]),
                                avl=float(a[7]), kbo=int(float(a[8])), kao=int(float(a[9])), asks=asks, bids=bids)
                     biglist.append(dat)
 
 
-    with open("data_file.json", "w", encoding='utf-8') as write_file:
-        json.dump(bigdict, write_file)
+    z = filename.split('/')
+    yr = z[6]
+    mon = z[7]
+    dy = z[8]
+    name = z[9]
+    nextpath = path2 + '/' + yr
+    if not os.path.exists(nextpath):
+        os.mkdir(nextpath)
+    nextpath = nextpath + '/' + mon
+    if not os.path.exists(nextpath):
+        os.mkdir(nextpath)
+    nextpath = nextpath + '/' + dy
+    if not os.path.exists(nextpath):
+        os.mkdir(nextpath)
+    fullname = nextpath + '/' + name
+
+    with open(fullname, 'w', encoding='utf-8') as fl:
+        json.dump(bigdict, fl)
+        print(fullname)
 
 
-timer=time.time()
+timer = time.time()
 if __name__ == '__main__':
     with multiprocessing.Pool(8) as pool:
         pool.map_async(perepars, content, callback=end_func)
